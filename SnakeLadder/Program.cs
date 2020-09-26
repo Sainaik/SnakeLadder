a9384a7c91c8f34d1f954f0c3db67aa0e1851cf5
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-09-26 19:20:24 +0530

    [Sai] Added player and postion

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -6,7 +6,17 @@ namespace SnakeLadder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Snake Ladder Game!");
+            Player p1 = new Player();
+
+            //Constants
+
+            const int INITIAL_POS = 0;
+
+            //variables
+
+            int position = 0;
+
+            Console.Out.WriteLine("Start position is: " + INITIAL_POS);
         }
     }
 }
