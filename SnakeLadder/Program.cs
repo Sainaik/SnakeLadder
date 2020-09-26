a82b6ff818adaa6d7a4754b6686875009565bd2d
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-09-26 20:37:51 +0530

    [Sai]Refactor. added rolling func

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -4,9 +4,16 @@ namespace SnakeLadder
 {
     class Program
     {
+
+        public static int GetPositon()
+        {
+            Random rand = new Random();
+
+            return rand.Next(0, 7);
+        }
         static void Main(string[] args)
         {
-            Player p1 = new Player();
+            String player1 = "Player1";
 
             //Constants
 
@@ -15,8 +22,16 @@ namespace SnakeLadder
             //variables
 
             int position = 0;
+          
 
             Console.Out.WriteLine("Start position is: " + INITIAL_POS);
+
+            position = INITIAL_POS +  GetPositon();
+
+            Console.Out.WriteLine("After the first roll: " + position);
+
+
+
         }
     }
 }
