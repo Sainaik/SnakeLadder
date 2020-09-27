089496b6f825722afe1f8528cb0efdf1269ebd69
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-09-27 17:06:46 +0530

    [Sai]commit

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -6,17 +6,8 @@ namespace SnakeLadder
     {
         static void Main(string[] args)
         {
-            Player p1 = new Player();
-
-            //Constants
-
-            const int INITIAL_POS = 0;
-
-            //variables
-
-            int position = 0;
-
-            Console.Out.WriteLine("Start position is: " + INITIAL_POS);
+           
+            Console.Out.WriteLine("Welcome to Snake Ladder Problem");
         }
     }
 }
